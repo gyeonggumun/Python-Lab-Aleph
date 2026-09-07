965102d2e41bd636a85cedabc82ef38f44b16971
gyeonggumun <lion989072@gmail.com>
2026-09-07 11:14:24 +0900

    자리 교체로 인한 설정 변경

--- report_maker.py
+++ report_maker.py
@@ -12,7 +12,7 @@ import requests  # pip install requests
 # 각자 본인 n8n 웹훅 주소를 넣으세요.
 # 환경변수 N8N_WEBHOOK_URL 이 설정돼 있으면 그 값을 우선 사용합니다.
 # WEBHOOK_URL = os.getenv("N8N_WEBHOOK_URL", "")   # ← 여기 또는 환경변수에
-WEBHOOK_URL = "http://localhost:5678/webhook-test/ced10dfe-0f09-4301-8aae-032798353e32"
+WEBHOOK_URL = "http://localhost:5678/webhook/dbf71751-63bc-4cdd-ab0f-52b729f353bd"
 
 BASE_DIR = Path(__file__).resolve().parent       # 스크립트가 있는 폴더 기준
 REPORT_PATH = BASE_DIR / "report.md"
