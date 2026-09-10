da2ca07376a569d5024face7b9a98b8ff26194d9
gyeonggumun <lion989072@gmail.com>
2026-09-10 14:58:01 +0900

    인가/권한 접근을 제어

--- _7_board_test/controllers/auth_controller.py
+++ _7_board_test/controllers/auth_controller.py
@@ -1,35 +1,60 @@
-"""회원가입 / 로그인."""
-from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token
-from werkzeug.security import check_password_hash, generate_password_hash
-
+from flask import Blueprint, render_template, request, redirect, url_for, flash, abort
+from flask_login import login_user, logout_user, login_required, current_user
+from models.user import User
 from extensions import db
-from models import User
+from functools import wraps
 
-auth_bp = Blueprint('auth', __name__, url_prefix='/api/auth')
+auth_bp = Blueprint('auth', __name__)
 
+# [추가됨] 권한 제어 데코레이터
+def requires_grade(minimum_grade):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.is_authenticated:
+                return redirect(url_for('auth.login'))
+            
+            # 요구 등급보다 낮으면 403 Forbidden 에러 발생
+            if current_user.grade < minimum_grade:
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
 
-@auth_bp.route('/register', methods=['POST'])
-def register():
-  data = request.get_json(silent=True) or {}
-  if not data.get('username') or not data.get('password'):
-    return jsonify({'msg': 'username, password 는 필수입니다.'}), 400
-  if User.query.filter_by(username=data['username']).first():
-    return jsonify({'msg': '이미 존재하는 사용자입니다.'}), 400
-
-  user = User(username=data['username'],
-              password=generate_password_hash(data['password']))
-  db.session.add(user)
-  db.session.commit()
-  return jsonify({'msg': '회원가입 성공'}), 201
-
-
-@auth_bp.route('/login', methods=['POST'])
+@auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-  data = request.get_json(silent=True) or {}
-  user = User.query.filter_by(username=data.get('username')).first()
-  if not user or not check_password_hash(user.password, data.get('password', '')):
-    return jsonify({'msg': '아이디 또는 비밀번호가 잘못되었습니다.'}), 401
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('page.index')) # 메인 화면으로 이동
+        flash('로그인 실패. 아이디나 비밀번호를 확인하세요.', 'danger')
+    return render_template('login.html')
 
-  token = create_access_token(identity=str(user.id))
-  return jsonify(access_token=token, username=user.username)
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if User.query.filter_by(username=username).first():
+            flash('이미 존재하는 사용자입니다.', 'warning')
+            return redirect(url_for('auth.register'))
+        
+        new_user = User(username=username)
+        new_user.set_password(password)
+        # 등급은 모델에서 default=0 이므로 자동 일반유저 가입
+        
+        db.session.add(new_user)
+        db.session.commit()
+        flash('회원가입 완료! 일반 유저로 가입되었습니다.', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html')
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('로그아웃 되었습니다.', 'info')
+    return redirect(url_for('page.index'))
