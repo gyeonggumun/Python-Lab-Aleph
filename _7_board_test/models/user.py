1655023e8778d4a794d643325b9437b8f53237ba
gyeonggumun <lion989072@gmail.com>
2026-09-10 14:57:11 +0900

    회원가입 시 기본 일반 유저로 가입

--- _7_board_test/models/user.py
+++ _7_board_test/models/user.py
@@ -1,12 +1,18 @@
 from extensions import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    
+    # [추가됨] 등급 컬럼 (0: 일반, 1: 골드, 2: 관리자) - 기본값 일반(0)
+    grade = db.Column(db.Integer, default=0, nullable=False)
 
-class User(db.Model):
-  __tablename__ = 'users'
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True, nullable=False)
-  password = db.Column(db.String(255), nullable=False)   # 해시만 저장(평문 금지)
-
-  def __repr__(self):
-    return f'<User {self.username}>'
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
